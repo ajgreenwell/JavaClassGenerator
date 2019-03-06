c1fe6af3c795de7d8e461a77e1f47560299551a7
ajgreenwell <greenwan@bc.edu>
2019-03-06 13:12:35 +0100

    added error dispatching system for command line arguments

--- src/generate.py
+++ src/generate.py
@@ -12,8 +12,26 @@ import settings
 import sys
 from handlers import handler_objects
 
+
+class CommandLineArgErrs():
+
+	__INTERFACE_ERROR_MSG = '***InvalidArgumentError*** : First Arg Must be a Valid Interface'
+	__INDENTATION_ERROR_MSG = None
+
+	__ERR_MESSAGES = {
+
+	'1': __INTERFACE_ERROR_MSG,
+	'2': __INDENTATION_ERROR_MSG
+
+	}
+
+	@classmethod
+	def get_err_msg(cls, arg_num):
+		return cls.__ERR_MESSAGES[str(arg_num)]
+
+
 # Checks each handler object to look for regex matches on provided line.
-def parse(line):
+def parse_interface_code(line):
 	for category, handler in handler_objects.items():
 		match = handler.match(line)
 		if match:
@@ -23,7 +41,7 @@ def parse(line):
 
 # If the user provided a relative path to the 
 # interface, return it. Else, return the null string.
-def get_path(interface_name):
+def get_file_path(interface_name):
 	path = ''
 	interface_has_path = '/' in interface_name
 	if interface_has_path:
@@ -32,36 +50,61 @@ def get_path(interface_name):
 	return path
 
 
-def main():
-	# get command line args and set variables accordingly
+def get_command_line_arg(arg_num):
+	arg = ''
 	try:
-		interface_name = sys.argv[1]
+		arg = sys.argv[arg_num]
+		return arg
 	except:
-		print('***InvalidArgumentError*** : First Arg Must be a Valid Interface', 
-			  file=sys.stderr)
-		exit(1)
-	try:
-		settings.num_spaces = int(sys.argv[2])
-	except:
-		pass # if user does not provide this argument, use default value from settings.py
+		err_msg = CommandLineArgErrs.get_err_msg(arg_num)
+		if err_msg:
+			print(err_msg, file=stderr)
+			exit(1)
 
-	path_to_interface = get_path(interface_name)
 
-	# open interface and class files for reading and writing
+def open_interface_file(filename, mode):
+	interface_file = ''
 	try:
-		interface_file = open(interface_name, 'r')
+		interface_file = open(filename, mode)
 	except:
 		print('***InvalidFilenameError*** : File or Relative Path Does Not Exist', 
 			  file=sys.stderr)
 		exit(1)
+	return interface_file
+
+
+def prompt_for_class_name():
 	class_name = input('Please enter the name of your class file: ')
+	isJavaFile = class_name[len(class_name) - 5 :] == '.java'
+	if not isJavaFile:
+		print('***UserInputError*** : Your class name must end in ".java"')
+		exit(1)
+	return class_name
+
+
+def main():
+	# get command line args and user input
+	interface_name = get_command_line_arg(1)
+	class_name = prompt_for_class_name()
+
+	num_spaces = get_command_line_arg(2)
+	if num_spaces:
+		settings._num_spaces = int(num_spaces)
+
+	# open interface and class files for reading and writing
+	interface_file = open_interface_file(interface_name, 'r')
+	path_to_interface = get_file_path(interface_name)
 	class_file = open(path_to_interface + class_name, 'a')
-	class_file.write(settings.comments.format(interface_name))
+
+	# file extension is no longer needed
 	class_name = class_name.rstrip('.java')
+
+	# write out boilerplate comments from settings.py
+	class_file.write(settings.comments.format(interface_name))
 
 	# main loop that parses each line of the interface
 	for line in interface_file:
-		match, handler = parse(line.rstrip('\n'))
+		match, handler = parse_interface_code(line.rstrip('\n'))
 		if match and handler:
 			code = handler.generate_code(match, class_name)
 			class_file.write(code)
